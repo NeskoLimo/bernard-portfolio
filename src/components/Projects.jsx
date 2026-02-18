b4386640f286ebb6c5fe8ec6963d78b0df6f6a5e
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-18 08:53:26 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      {/* Your projects content here */}
+    </div>
+  );
+};
+
+export default Projects;
