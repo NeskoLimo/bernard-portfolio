987ad2c7f659f268e35fa5595fcd81ecb29bf9ad
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-18 15:14:46 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Bernard. All rights reserved.</p>
+      <p>&copy; 2026 Bernard. All rights reserved.</p>
     </footer>
   );
 };
