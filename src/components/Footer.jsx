6e50339a639d8d23a9a2fff42a64d74c797fe0ec
Benard Kones <benard.kones@smart.local>
2026-02-18 09:30:13 +0300

    add Footer and Contact components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; 2024 Bernard. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
+ 
