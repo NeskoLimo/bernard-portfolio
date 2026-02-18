ab8dcddcc7007360e6ea57edabedb42c64752dd8
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-18 20:24:08 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer>
-      <p>&copy; 2026 Bernard. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer; 
