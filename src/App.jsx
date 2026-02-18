2bf9e811a5e9c52079b7452c2f82a5860e4b9f88
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-19 00:54:28 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 // src/App.jsx
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Certifications from './components/Certifications';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
+import Home   from './components/Home';
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white">
       <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Certifications />
-      <Blog />
-      <Contact />
+      <Home />
     </div>
   );
 }
