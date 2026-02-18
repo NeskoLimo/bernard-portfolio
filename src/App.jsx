0e29a69d47b2d6968dccee8c7f99edf21ff1daab
Bernard Limo <127617357+NeskoLimo@users.noreply.github.com>
2026-02-18 23:21:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar     from './components/Navbar';
 import Home       from './components/Home';
+import Certifications from './components/Certifications';
 
 /**
  * App
